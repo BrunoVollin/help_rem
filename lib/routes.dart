122d2ac8b7c1c1ded91f97f270e19b172bc431dc
Bruno <bruno.3av@gmail.com>
2021-08-03 10:57:15 -0300

    create routes

---
+++ lib/routes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:help_rem/theme/blue.dart';
+import 'package:help_rem/views/create_account_page.dart';
+import 'package:help_rem/views/create_dear_one.dart';
+import 'package:help_rem/views/dear_one_list.dart';
+import 'package:help_rem/views/dear_one_specific.dart';
+import 'package:help_rem/views/login_page.dart';
+import 'package:help_rem/views/medicine_page.dart';
+import 'package:help_rem/views/menu_page.dart';
+
+class Routes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var themeData = ThemeData(
+      primarySwatch: blueTheme(),
+    );
+
+    return MaterialApp(
+        theme: themeData,
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => MenuPage(),
+          '/dear_one_list': (context) => DearOneList(),
+          '/dear_one_specific': (context) => DearOneSpecific(),
+          '/create_account_page': (context) => CreateAccount(),
+          '/create_dear_one': (context) => CreateDearOne(),
+          '/medicine_page': (context) => MedicinePage(),
+          // '/food_page': (context) => FoodPage(),
+        });
+  }
+}
